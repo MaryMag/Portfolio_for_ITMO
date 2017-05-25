30cc731e0b92c81437579e1b362ffa4b9f5e977d
MaryMag <magnitova@gmail.com>
2017-05-25 23:59:37 +0300

    slider adaptive ok

--- src/js/script.js
+++ src/js/script.js
@@ -10,14 +10,63 @@ $(document).ready(function(){
     return false;
   });
 
-  $('.slider').slick({
+  $('.responsive').slick({
     dots: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     infinite: true, 
     speed: 300,
     autoplay: true, 
-    autoplaySpeed: 4000});
+    autoplaySpeed: 4000,
+    responsive: [
+      {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+
+    // {
+    //   breakpoint: 768,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1
+    //   }
+    // }
+    // {
+    //   breakpoint: 1000,
+    //   settings: {
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1
+    //   }
+    // }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+
+  // });
 
 });
 
